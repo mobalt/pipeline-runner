1c77681c75790987890990c3d0169afef38887b7
Moises Baltazar <me@moisesb.com>
2020-09-10 18:22:10 -0500

    Add load variables executor function

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 
+class VariableSetNotDefined(Exception):
+    def __init__(self, filename, variable_set_name):
+        super().__init__(f'The variable set "{variable_set_name}" is not defined in "{filename}".')
+
+class PipelineNotDefined(Exception):
+    def __init__(self, filename, pipeline):
+        super().__init__(f'The pipeline "{pipeline}" is not defined in "{filename}".')
+
+
 class FunctionNotDefined(Exception):
     def __init__(self, filename, function_name):
         super().__init__(f'The function "{function_name}" is not defined in "{filename}".')
@@ -15,6 +24,28 @@ class FunctionNotDefined(Exception):
 class VariableNotSet(Exception):
     def __init__(self, variables):
         super().__init__(f'The following variable(s) have not been set: {", ".join(variables)}.')
+
+class Pipelines:
+    def __init__(self, filename):
+        self.filename = filename
+        self.pipelines = load_yaml(filename)
+
+    def tasks(self, pipeline):
+        if pipeline not in self.pipelines:
+            raise PipelineNotDefined(self.filename, self.pipelines)
+
+        return self.pipelines[pipeline]
+
+class VariableLoader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.variable_sets = load_yaml(filename)
+
+    def load_set(self, variable_set_name):
+        if variable_set_name not in self.variable_sets:
+            raise VariableSetNotDefined(self.filename, self.variable_sets)
+
+        return self.variable_sets[variable_set_name]
 
 
 class Functions:
@@ -67,7 +98,8 @@ def load_yaml(filename):
 
 
 def load_pipeline(configuration_dir, pipeline_name, args):
-    pass
+    variables = {}
+    return
 
 
 expansion_regex = re.compile(r'\$([a-zA-Z0-9_]+)|\$\{([a-zA-Z0-9_]+)(?:\:([^}]*))?\}')
@@ -93,6 +125,49 @@ def shellexpansion(string, variables=None):
 
 def shellexpansion_dict(obj, variables=None):
     return {k: shellexpansion(v, variables) for k, v in obj.items()}
+
+
+class Executor:
+    def __init__(self, variables:dict, functions:Functions, templates:TemplateEnv, var_loader:VariableLoader):
+        self.variables = variables
+        self.functions = functions
+        self.templates = templates
+        self.var_loader = var_loader
+
+    @staticmethod
+    def from_config_dir(configuration_dir):
+        executor = Executor(
+            dict(os.environ),
+            Functions(f'{configuration_dir}/functions.py'),
+            TemplateEnv(f'{configuration_dir}/templates'),
+            VariableLoader(f'{configuration_dir}/variables.yaml'),
+        )
+        return executor
+
+    def load_variables(self, set_name):
+        if type(set_name) != str:
+            raise TypeError("Expecting a string with the set of variables to load. Instead received: ", set_name)
+
+        raw_variables = self.var_loader.load_set(set_name)
+        expanded_variables = shellexpansion_dict(raw_variables, self.variables)
+        updated_variables = {
+            **self.variables,
+            **expanded_variables
+        }
+        self.variables = updated_variables
+
+
+def task_load_variables(args, variables:dict, functions:Functions, templates:TemplateEnv, var_loader:VariableLoader):
+    if type(args) != str:
+        raise TypeError("Expecting a string with the set of variables to load. Instead received: ", args)
+
+    raw_variables = var_loader.load_set(args)
+    expanded_variables = shellexpansion_dict(raw_variables, variables)
+    return {
+        **variables,
+        **expanded_variables
+    }
+
 
 if __name__ == '__main__':
     pass
---
+++ tests/test_pipeline.py
@@ -0,0 +1,25 @@
+import main
+import pytest
+
+CONFIG_DIR = "example"
+
+def test_load_pipeline():
+    pipelines = main.Pipelines(f"{CONFIG_DIR}/pipelines.yaml")
+    assert pipelines.tasks("structural") != []
+
+@pytest.fixture
+def exec():
+    return main.Executor.from_config_dir(CONFIG_DIR)
+
+def test_executor_load_variables(exec):
+    before = dict(exec.variables)
+    exec.load_variables('functional')
+    after = dict(exec.variables)
+    assert len(before) < len(after)
+
+def test_executor_load_variables_has_expansion(exec):
+    exec.load_variables('functional')
+    raw = "$HOME/XNAT_BUILD_DIR/$USER"
+    actual = exec.variables['XNAT_PBS_JOBS_BUILD_DIR']
+    assert raw != actual
+
