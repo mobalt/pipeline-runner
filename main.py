d08f1e27466aabd7b0beb9a72da709b4bd70aea9
Moises Baltazar <me@moisesb.com>
2020-09-11 10:10:41 -0500

    Add ability to parse arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+import argparse
 import importlib.util
 import os
 import re
@@ -155,6 +156,9 @@ class Executor:
         self.functions = functions
         self.templates = templates
         self.var_loader = var_loader
+        self.config_dir = ""
+        self.dry_run = False
+        self.verbose = False
 
     @staticmethod
     def from_config_dir(configuration_dir):
@@ -164,6 +168,7 @@ class Executor:
             TemplateEnv(f"{configuration_dir}/templates"),
             VariableLoader(f"{configuration_dir}/variables.yaml"),
         )
+        executor.config_dir = configuration_dir
         return executor
 
     def load_variables(self, set_name):
@@ -226,6 +231,66 @@ class Executor:
         return expanded
 
 
+arg_pattern = re.compile("--([a-zA-Z0-9_]+)(?:=(.+))?")
+
+
+def parse_rest_of_args(args):
+    positional = []
+    named = {}
+    for x in args:
+        match = arg_pattern.match(x)
+        if match:
+            named[match.group(1)] = match.group(2) or ""
+        else:
+            positional.append(x)
+    escape_spaces = [x if " " not in x else f'"{x}"' for x in positional]
+    arg_zero = " ".join(escape_spaces)
+    positional = [arg_zero] + positional
+    positional_dict = {str(i): value for i, value in enumerate(positional)}
+    return {**positional_dict, **named}
+
+
+def parse_arguments(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        help="The configuration directory to use. Default is $PWD.",
+    )
+    parser.add_argument(
+        "--verbose", "-v", action="store_true", help="Verbose (for debugging pipeline)."
+    )
+    parser.add_argument(
+        "--dryrun",
+        "-n",
+        action="store_true",
+        help="Dry-run. Don't execute local scripts.",
+    )
+    parser.add_argument(
+        "PIPELINE", help="The name of the pipeline to run", default="DEFAULT"
+    )
+    parser.add_argument(
+        "ARGS",
+        help="The rest of the args get passed to the pipeline.",
+        nargs=argparse.REMAINDER,
+    )
+    parsed_args = parser.parse_args(args)
+    config_dir = (
+        os.path.abspath(parsed_args.config) if parsed_args.config else os.getcwd()
+    )
+    print(config_dir, parsed_args)
+    executor = Executor.from_config_dir(config_dir)
+    executor.verbose = parsed_args.verbose
+    executor.dry_run = parsed_args.dryrun
+    executor.variables["PIPELINE_NAME"] = parsed_args.PIPELINE
+    executor.variables.update(parse_rest_of_args(parsed_args.ARGS))
+
+    return executor
+
+
 if __name__ == "__main__":
-    pass
+    args = parse_arguments()
+    print(args)
+
     # load_pipeline()
