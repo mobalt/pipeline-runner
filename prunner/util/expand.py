192470675116de5a9c01e02067f1f9e7fdda468e
Moises Baltazar <me@moisesb.com>
2020-09-17 21:58:41 -0500

    Add ability to shellexpand all types, not just dict/str

--- prunner/util/expand.py
+++ prunner/util/expand.py
@@ -34,6 +34,19 @@ def shellexpansion(input_str, variables):
 
     return SHELL_VARIABLES_PATTERN.sub(replacements, input_str)
 
+def shellexpansion_list(array, variables):
+    return [shellexpansion_dict(v, variables) for v in array]
 
 def shellexpansion_dict(obj, variables):
-    return {k: shellexpansion(v, variables) for k, v in obj.items()}
+    accumulator = {}
+    for k, v in obj.items():
+        if type(v) == str:
+            accumulator[k] = shellexpansion(v, variables)
+        elif type(v) == dict:
+            accumulator[k] = shellexpansion_dict(v, variables)
+        elif type(v) == list:
+            accumulator[k] = shellexpansion_list(v, variables)
+        else:
+            accumulator[k] = v
+
+    return accumulator
