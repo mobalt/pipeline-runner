a951f1c9c9130c987efff29b2c432d57266f0ca7
Moises Baltazar <me@moisesb.com>
2020-09-30 09:11:13 -0500

    Bump to v0.2.4

--- prunner/exec/executor.py
+++ prunner/exec/executor.py
@@ -49,8 +49,8 @@ class Executor:
             if updates is None or type(updates) != dict:
                 updates = {}
             if self.env.verbose:
-                new_variables = {k:v for k, v in updates if k not in self.env.variables}
-                mutations = {k:v for k, v in updates if k in self.env.variables}
+                new_variables = {k:v for k, v in updates.items() if k not in self.env.variables}
+                mutations = {k:v for k, v in updates.items() if k in self.env.variables}
                 print("Mutations = ", mutations)
                 print("New Values = ", new_variables)
             self.env.variables = {
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="prunner",
-    version="0.2.3",
+    version="0.2.4",
     author="Moises Baltazar Garcia",
     author_email="me@moisesb.com",
     description="Runs pipelines configured in YAML",
