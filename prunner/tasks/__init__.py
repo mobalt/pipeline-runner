5b32f4c35041703c536731049b5132bce4b19819
Moises Baltazar <me@moisesb.com>
2020-10-06 19:37:00 -0500

    Bump to v0.3.7

--- prunner/tasks/__init__.py
+++ prunner/tasks/__init__.py
@@ -3,10 +3,12 @@ from .call_function import FunctionTask, ParamsNotDefined
 from .generate_file import GenerateFileTask
 from .load_variables import LoadVariablesTask
 from .set_variables import SetVariablesTask
+from .dump_variables import DumpVarsTask
 
 STANDARD_TASKS = (
     LoadVariablesTask,
     SetVariablesTask,
     FunctionTask,
     GenerateFileTask,
+    DumpVarsTask,
 )
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="prunner",
-    version="0.3.6",
+    version="0.3.7",
     author="Moises Baltazar Garcia",
     author_email="me@moisesb.com",
     description="Runs pipelines configured in YAML",
