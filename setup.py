31087db7b3734a24ddc664402e84a3015212e844
Moises Baltazar <me@moisesb.com>
2020-10-28 07:44:09 -0500

    Bump to v0.3.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="prunner",
-    version="0.3.10",
+    version="0.3.11",
     author="Moises Baltazar Garcia",
     author_email="me@moisesb.com",
     description="Runs pipelines configured in YAML",
